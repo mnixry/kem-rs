4ec5e5b65c40ec33c95b54cc2d0e76cb19f0fe27
HexMix <32300164+mnixry@users.noreply.github.com>
2026-02-20 23:57:49 +0800

    ci: add GitHub Actions workflow for Nix Flake checks

--- flake.nix
+++ flake.nix
@@ -1,52 +1,102 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
+    flake-parts = {
+      url = "github:hercules-ci/flake-parts";
+      inputs.nixpkgs-lib.follows = "nixpkgs";
+    };
     rust-overlay = {
       url = "github:oxalica/rust-overlay";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    treefmt-nix = {
+      url = "github:numtide/treefmt-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    crane.url = "github:ipetkov/crane";
   };
 
   outputs =
-    {
-      nixpkgs,
-      flake-utils,
-      rust-overlay,
-      ...
-    }:
-    flake-utils.lib.eachDefaultSystem (
-      system:
-      let
-        inherit (nixpkgs) lib;
-        pkgs = import nixpkgs {
-          inherit system;
-          overlays = [ (import rust-overlay) ];
+    { flake-parts, ... }@inputs:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = inputs.nixpkgs.lib.systems.flakeExposed;
+      imports = [ inputs.treefmt-nix.flakeModule ];
+      perSystem =
+        { lib, system, ... }:
+        let
+          pkgs = import inputs.nixpkgs {
+            inherit system;
+            overlays = [ (import inputs.rust-overlay) ];
+          };
+          rust = pkgs.rust-bin.selectLatestNightlyWith (
+            toolchain:
+            toolchain.default.override {
+              extensions = [
+                "rust-src"
+                "llvm-tools-preview"
+              ];
+            }
+          );
+          craneLib = (inputs.crane.mkLib pkgs).overrideToolchain (_: rust);
+        in
+        {
+          _module.args = { inherit inputs pkgs rust; };
+          treefmt =
+            { ... }:
+            {
+              projectRootFile = ".git/config";
+              programs.nixfmt.enable = true;
+              programs.taplo.enable = true;
+              programs.rustfmt = {
+                enable = true;
+                package = rust;
+              };
+              programs.yamlfmt.enable = true;
+            };
+          devShells.default = pkgs.mkShell {
+            buildInputs =
+              lib.singleton rust
+              ++ (with pkgs; [
+                cargo-edit
+                cargo-nextest
+                cargo-llvm-cov
+                cargo-criterion
+                cargo-pgo
+                llvmPackages.bolt
+                gnuplot
+                taplo
+              ]);
+          };
+          checks =
+            let
+              commonArgs = (craneLib.crateNameFromCargoToml { cargoToml = ./crates/lib/Cargo.toml; }) // rec {
+                src = ./.;
+                strictDeps = true;
+                cargoVendorDir = craneLib.vendorMultipleCargoDeps {
+                  inherit (craneLib.findCargoFiles src) cargoConfigs;
+                  cargoLockList = [
+                    ./Cargo.lock
+                    "${rust}/lib/rustlib/src/rust/library/Cargo.lock"
+                  ];
+                };
+              };
+              cargoArtifacts = craneLib.buildDepsOnly commonArgs;
+            in
+            {
+              clippy = craneLib.cargoClippy (
+                commonArgs
+                // {
+                  inherit cargoArtifacts;
+                  cargoClippyExtraArgs = "--all-targets --all-features";
+                }
+              );
+              test = craneLib.cargoNextest (
+                commonArgs
+                // {
+                  inherit cargoArtifacts;
+                }
+              );
+            };
         };
-        rust = pkgs.rust-bin.selectLatestNightlyWith (
-          toolchain:
-          toolchain.default.override {
-            extensions = [
-              "rust-src"
-              "llvm-tools-preview"
-            ];
-          }
-        );
-      in
-      {
-        devShell = pkgs.mkShell {
-          buildInputs =
-            lib.singleton rust
-            ++ (with pkgs; [
-              cargo-nextest
-              cargo-llvm-cov
-              cargo-criterion
-              cargo-pgo
-              llvmPackages.bolt
-              gnuplot
-              taplo
-            ]);
-        };
-      }
-    );
+    };
 }
